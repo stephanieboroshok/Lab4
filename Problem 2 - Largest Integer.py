78bb9885c0f00e6335f3ff9cf6555a5e71af9a1c
stephanieboroshok <sb8485a@american.edu>
2019-09-18 10:21:13 -0400

    Actually using function

--- Problem 2 - Largest Integer.py
+++ Problem 2 - Largest Integer.py
@@ -1,18 +1,16 @@
 # Create function that takes 3 integers and returns the greatest
 
 def largestInt (num1, num2, num3):
-    num1 = int(input('Enter a number:'))
-    num2 = int(input('Enter a number:'))
-    num3 = int(input('Enter a number:'))
+    if ((num1 > num2) and (num1 > num3)):
+        print('The largest integer is num1:', num1)
+    elif ((num2 > num1) and (num2 > num3)):
+        print('The largest integer is num2:', num2)
+    if ((num3 > num1) and (num3 > num2)):
+        print('The largest integer is num3:', num3)
+    else:
+        pass
 num1 = int(input('Enter a number:'))
 num2 = int(input('Enter a number:'))
 num3 = int(input('Enter a number:'))
 
-if ((num1>num2) and (num1>num3)):
-    print ('The largest integer is num1:', num1)
-elif ((num2>num1) and (num2>num3)):
-    print ('The largest integer is num2:', num2)
-if ((num3>num1) and (num3>num2)):
-    print ('The largest integer is num3:', num3)
-else:
-    pass
+print (largestInt(num1, num2, num3))
