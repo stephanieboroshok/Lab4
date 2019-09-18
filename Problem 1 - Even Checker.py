76ac19512bf6a21e3556bc59adabefa0b2f3588b
stephanieboroshok <sb8485a@american.edu>
2019-09-18 10:26:25 -0400

    corrected to use the function and added notes

--- Problem 1 - Even Checker.py
+++ Problem 1 - Even Checker.py
@@ -2,12 +2,17 @@
 # and returns true if the integer is even, false if odd
 
 def isEven (number1):
-    number1 = int(input('Enter a number:'))
+    #define function
+    if (number1 % 2 == 0):
+        # modulus operator (%) shows remainder after dividing by 2
+        # if remainder = 0, then integer must be even
+        print('The integer is even.')
+    else:
+        print('The integer is odd.')
 
 number1 = int(input('Enter a number:'))
-if (number1%2 == 0):
-    # modulus operator (%) shows remainder after dividing by 2
-    # if remainder = 0, then integer must be even
-    print ('The integer is even.')
-else:
-    print ('The integer is odd.')
+# this is the first thing to run
+# collects input for number
+
+print (isEven(number1))
+# running the function
